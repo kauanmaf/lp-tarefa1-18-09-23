66755ea39312d31729c55b99b535dbe06588de28
HENZO FELIPE CARVALHO DE MATTOS <B51104@fgv.edu.br>
2023-09-18 08:46:30 -0300

    mudanças pequenas no modulo diferença

--- calcula_diferenca.py
+++ calcula_diferenca.py
@@ -21,13 +21,17 @@ def data_diferenca(datas):
 
     Exemplos:
         
-        >>>data_diferença( ('2023-09-08', '2023-10-20'))
+        >>>data_diferenca( ('2023-09-08', '2023-10-20') )
+        42
         
     """
     
+    # Converte as datas para o formato date
     d_data_1 = date.fromisoformat(datas[0])
     d_data_2 = date.fromisoformat(datas[1])
     
+    #Subtrai a diferença
     diff = d_data_2 - d_data_1
     
+    #Retorna o valor absoluto
     return (abs(diff.days))
