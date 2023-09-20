d7b8c5c757cdf80d649be4c5754bdfede81893d9
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-09-20 09:09:23 -0300

    corrigindo teste

--- testes_sillas.py
+++ testes_sillas.py
@@ -1,21 +1,20 @@
-
-
-
 import unittest
 from unittest.mock import patch
 import auxiliares
 
+class TestAuxiliares(unittest.TestCase):
 
-class Auxiliares(unittest.TestCase):
-    def leitura_arquivo(self):
-        self.assertEqual(auxiliares.le_arquivo("data.csv"), "29 de Janeiro de 2001 - 30 de Janeiro de 2001")
+    def test_leitura_arquivo(self):
+        expected_result = "29 de Janeiro de 2001 - 30 de Janeiro de 2001"
+        actual_result = auxiliares.le_arquivo("data.csv")
+        self.assertEqual(actual_result, expected_result, "File reading result is incorrect")
 
-    def mes_invalido(self):
-        self.assertEqual(auxiliares.return_date("12 de catacra de 1324 - 15 de maio de 2023"),
-                         "12 de catacra de 1324 - 15 de maio de 2023")
-
+    @unittest.expectedFailure
+    def test_mes_invalido(self):
+        invalid_date = "12 de catacra de 1324 - 15 de maio de 2023"
+        result = auxiliares.return_date(invalid_date)
+        self.assertEqual(result, invalid_date, "Expected failure: Invalid date format")
 
 if __name__ == "__main__":
     unittest.main()
-
 
