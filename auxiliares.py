c1b3d8fdafe9bfdfb6ac67c251c6cfd8f3e22701
GABRIEL PAVANATO <B51118@fgv.edu.br>
2023-09-20 07:49:27 -0300

    Modificações simples

--- auxiliares.py
+++ auxiliares.py
@@ -49,54 +49,31 @@ def return_date(date_input):
         
     ------
     
-    Esta função auxiliar pega uma string e converte ela no formato ISO 8601 para se calcular a distância entre
+    Esta função pega uma string e converte ela no formato ISO 8601 para se calcular a distância entre
     duas datas.
+    
+    Exemplo:
+        
+    >>> return_date("12 de dezembro de 1324 - 15 de maio de 2023")
+    ('1324-12-12', '2023-05-15')
+    
+    >>> return_date("1 de janeiro de 2000 - 31 de dezembro de 2000")
+    ('2000-01-1', '2000-12-31')
     """
     
     # Transforma o input numa lista de strings
     date_input_list = date_input.split(maxsplit=11)
     
-    months_dict = {'Janeiro': '01', 'Fevereiro': '02', 'Março': '03', 'Abril': '04', 'Maio': '05', 'Junho': '06', 
-                   'Julho': '07', 'Agosto': '08', 'Setembro': '09', 'Outubro': '10', 'Novembro': '11', 'Dezembro': '12'}
+    months_dict = {'janeiro': '01', 'fevereiro': '02', 'março': '03', 'abril': '04', 'maio': '05', 'junho': '06', 
+                   'julho': '07', 'agosto': '08', 'setembro': '09', 'outubro': '10', 'novembro': '11', 'dezembro': '12'}
     
     # Cria duas listas de cada data
-    date_1_list = [date_input_list[0], date_input_list[2], date_input_list[4]]
-    date_2_list = [date_input_list[6], date_input_list[8], date_input_list[10]]
+    date_1_list = [date_input_list[0], date_input_list[2].lower(), date_input_list[4]]
+    date_2_list = [date_input_list[6], date_input_list[8].lower(), date_input_list[10]]
+        
     
     # Coloca as datas no formato ISO 8601
     date_1_str =  date_1_list[2] + '-' + months_dict[date_1_list[1]] + '-' + date_1_list[0]
     date_2_str =  date_2_list[2] + '-' + months_dict[date_2_list[1]] + '-' + date_2_list[0]
     
     return (date_1_str, date_2_str)
-
-
-def data_diferenca(datas):
-    """
-    Parameters
-    ----------
-    (datas) : string tuple
-        Datas inicial e final
-
-    Returns
-    -------
-    TYPE: int
-        Diferença das datas
-
-    -------
-    
-    Esta função calcula a diferença entre duas datas dadas como tuplas
-    no formato ISO 8601 (YYYY-MM-DD)
-    
-    Exemplos:
-        
-        >>> data_diferenca(('2023-09-08', '2023-10-20'))
-        42
-        
-    """
-    
-    d_data_1 = date.fromisoformat(datas[0])
-    d_data_2 = date.fromisoformat(datas[1])
-    
-    diff = d_data_2 - d_data_1
-    
-    return (abs(diff.days))
